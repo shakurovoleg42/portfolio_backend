ccf159d51a3a0a0e42eb8957de606d3f13c08912
Oleg Shakurov <thorsonodin2003@gmail.com>
2025-01-30 15:04:29 +0500

    adminka done

--- mongo.js
+++ mongo.js
@@ -1,18 +1,43 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
-const { v4: uuidv4 } = require("uuid");
+const slugify = require("slugify");
 
-const portfolioSchema = new mongoose.Schema({
-  id: {
-    type: String,
-    default: uuidv4,
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+    process.exit(1);
+  }
+};
+
+// Схема и модель для портфолио
+const portfolioSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: String,
+    technologies: [String],
+    slug: { type: String, unique: true },
+    list: [String],
+    images: [String],
   },
-  title: String,
-  description: String,
-  technologies: [String],
-  list: [String],
-  images: [String],
+  { collection: "portfolioDB" }
+);
+
+// Middleware для создания уникального slug
+portfolioSchema.pre("save", async function (next) {
+  if (this.isNew) {
+    let slug = slugify(this.title, { lower: true, strict: true });
+    const slugExists = await Portfolio.findOne({ slug });
+    if (slugExists) {
+      slug = `${slug}-${Date.now()}`;
+    }
+    this.slug = slug;
+  }
+  next();
 });
 
 const Portfolio = mongoose.model("Portfolio", portfolioSchema);
 
-module.exports = Portfolio;
+module.exports = { connectDB, Portfolio };
